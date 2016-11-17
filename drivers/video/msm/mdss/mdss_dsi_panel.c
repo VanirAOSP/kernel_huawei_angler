1250be3a9e50693acf5d9910a94a3e390d62c622
nuclearmistake <nuclearmistake@gmail.com>
2016-11-16 20:33:47 -0500

    Reconcile with CM because because
    Change-Id: Ief70aaf45df9c3770eb02db0f609ba7cd85c1d56

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1858,8 +1858,6 @@ static int mdss_panel_parse_dt(struct device_node *np,
 
 	mdss_dsi_parse_dynamic_dsitiming_config(np, ctrl_pdata);
 
-	mdss_livedisplay_parse_dt(np, pinfo);
-
 	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->lp_on_cmds,
 		"qcom,mdss-dsi-lp-mode-on", NULL);
 
